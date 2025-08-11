32b234fcb7cb62e024cbc831700c556e4a075d7a
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2025-08-11 12:31:48 +0530

    Update PickerHandler.iOS.cs

--- src/Core/src/Handlers/Picker/PickerHandler.iOS.cs
+++ src/Core/src/Handlers/Picker/PickerHandler.iOS.cs
@@ -38,9 +38,6 @@ namespace Microsoft.Maui.Handlers
 		protected override MauiPicker CreatePlatformView() =>
 			new MauiPicker(null) { BorderStyle = UITextBorderStyle.RoundedRect };
 
-
-
-
 		void DisplayAlert(MauiPicker uITextField)
 		{
 			var paddingTitle = 0;
