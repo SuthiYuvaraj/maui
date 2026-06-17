0767d19db8ef8601c54c5d6853ae0bb2be5078ed
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2026-06-17 22:06:34 +0530

    Update MotionEventExtensions.cs

--- src/Controls/src/Core/Platform/Android/Extensions/MotionEventExtensions.cs
+++ src/Controls/src/Core/Platform/Android/Extensions/MotionEventExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Android.Views;
+﻿using System;
+using Android.Views;
 using Microsoft.Maui.Graphics;
 using AView = Android.Views.View;
 
@@ -46,5 +47,37 @@ internal static class MotionEventExtensions
 		}
 
 		return null;
+	}
+
+	internal static Func<IElement?, Point?> CapturePosition(this MotionEvent? e, IElement? sourceElement)
+	{
+		var context = sourceElement?.Handler?.MauiContext?.Context;
+		if (context == null || e == null)
+			return _ => null;
+
+		var rawX = e.RawX;
+		var rawY = e.RawY;
+		var localX = e.GetX();
+		var localY = e.GetY();
+
+		return (relativeElement) =>
+		{
+			if (relativeElement == null)
+				return new Point(context.FromPixels(rawX), context.FromPixels(rawY));
+
+			if (relativeElement == sourceElement)
+				return new Point(context.FromPixels(localX), context.FromPixels(localY));
+
+			if (relativeElement.Handler?.PlatformView is AView aView)
+			{
+				var location = aView.GetLocationOnScreenPx();
+				var x = rawX - location.X;
+				var y = rawY - location.Y;
+
+				return new Point(context.FromPixels(x), context.FromPixels(y));
+			}
+
+			return null;
+		};
 	}
 }
